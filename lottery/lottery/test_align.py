b7959a52f73b79d333e83bc142dd896382bf0942
huangarthur89 <huangsteve8899@gmail.com>
2026-06-04 14:48:03 +0800

    Delete lottery directory

--- lottery/lottery/test_align.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-df = pd.DataFrame({'策略名稱': ['A', 'B'], '第 1 球': [1, 2], '第 2 球': [3, 4]})
-styled = df.style.set_properties(**{'text-align': 'center'})
-styled = styled.set_table_styles([{'selector': 'th', 'props': [('text-align', 'center')]}])
-st.markdown(styled.hide(axis="index").to_html(), unsafe_allow_html=True)
--- lottery/test_md.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-md = """
-| 策略名稱 | 第 1 球 | 第 2 球 | 第 3 球 | 第 4 球 | 第 5 球 | 第 6 球 | 特別號 | 預計中獎金額 |
-| :--- | :---: | :---: | :---: | :---: | :---: | :---: | :---: | :--- |
-| 🔥 歷史最熱門 | <span style='color:#FF4B4B; font-weight:bold;'>2</span> | 8 | 15 | 23 | 35 | 41 | 37 | $0 |
-"""
-st.markdown(md, unsafe_allow_html=True)
